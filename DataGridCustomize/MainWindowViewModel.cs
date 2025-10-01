b90c931da57c5356df62c862e57b65f3e7d9aa6c
rkitamu <rkitamun@gmail.com>
2025-10-02 08:35:03 +0900

    プロジェクト ファイルを追加します。

---
+++ DataGridCustomize/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridCustomize
+{
+    public partial class MainWindowViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<UserViewModel> _users;
+
+        public MainWindowViewModel()
+        {
+            _users = new ObservableCollection<UserViewModel>
+            {
+                new UserViewModel
+                {
+                    Id = 101,
+                    Name = "Taro Suzuki",
+                    Description = "He is a software engineer.",
+                    IsNative = false
+                },
+                new UserViewModel
+                {
+                    Id = 102,
+                    Name = "Hanako Yamada",
+                    Description = "She is a designer.",
+                    IsNative = false
+                },
+                new UserViewModel
+                {
+                    Id = 103,
+                    Name = "Jiro Sato",
+                    Description = "He is a project manager.",
+                    IsNative = false
+                }
+            };
+        }
+    }
+}
---
+++ DataGridCustomize/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridCustomize
+{
+    public partial class UserViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private int _id;
+
+        [ObservableProperty]
+        private string _name;
+
+        [ObservableProperty]
+        private string _description;
+
+        [ObservableProperty]
+        private bool _isNative;
+    }
+}
